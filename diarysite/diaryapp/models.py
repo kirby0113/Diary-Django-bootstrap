19e24d48683dfaf986850278cf83df424ca0faed
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-07-14 20:45:33 +0900

    adminでの表示用に修正

--- diarysite/diaryapp/models.py
+++ diarysite/diaryapp/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Diary(models.Model):
+    def __str__(self):
+        return self.title
     title = models.CharField(max_length=100)
     date = models.DateField()
     author = models.CharField(max_length=100)
