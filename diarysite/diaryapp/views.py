d8879e10c150fafccc13e835f6921f70e59b7cb6
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-07-24 13:02:44 +0900

    キャッシュ整理

--- diarysite/diaryapp/views.py
+++ diarysite/diaryapp/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Diary
-
+import datetime
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        title = request.POST.get("title",)
+        author = request.POST.get("author",)
+        content = request.POST.get("content",)
+        image = request.FILES["image"]
+        date = datetime.date.today()
+
+        diary = Diary(title=title,author=author,content=content,date=date,image=image)
+        diary.save()
+        return redirect("/")
     DiaryList = Diary.objects.all()
     return render(request,"diaryapp/index.html",{"DiaryList":DiaryList})
